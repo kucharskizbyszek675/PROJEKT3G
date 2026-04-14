64ff408b3499deebf72a14fe82b9f2cd7f98ea0d
kucharskizbyszek675 <kucharskizbyszek675@gmail.com>
2026-04-14 22:19:52 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,91 @@
+function opinia1(){
+    var nick = document.getElementById("User").value
+    var ogrze = document.getElementById("ogame").value
+    document.getElementById("txt").innerHTML = "Dziękujemy za twoją opinie " + nick + ". Miło nam że korzystasz z naszego serwisu"
+    if(nick == "" || nick == null){
+        alert("WYPEŁNIJ WSZYSTKIE POLA")
+        document.getElementById("txt").innerHTML = ""
+    }
+    if(ogrze == "" || ogrze == null){
+        alert("WYPEŁNIJ WSZYSTKIE POLA")
+        document.getElementById("txt").innerHTML = ""
+    }
+}
+function opinia2(){
+    var nick1 = document.getElementById("User1").value
+    var ogrze1 = document.getElementById("ogame1").value
+    document.getElementById("txt1").innerHTML = "Dziękujemy za twoją opinie " + nick1 + ". Miło nam że korzystasz z naszego serwisu"
+    if(nick1 == "" || nick1 == null){
+        alert("WYPEŁNIJ WSZYSTKIE POLA")
+        document.getElementById("txt1").innerHTML = ""
+    }
+    if(ogrze1 == "" || ogrze1 == null){
+        alert("WYPEŁNIJ WSZYSTKIE POLA")
+        document.getElementById("txt1").innerHTML = ""
+    }
+}
+function opinia3(){
+    var nick2 = document.getElementById("User2").value
+    var ogrze2 = document.getElementById("ogame2").value
+    document.getElementById("txt2").innerHTML = "Dziękujemy za twoją opinie " + nick2 + ". Miło nam że korzystasz z naszego serwisu"
+    if(nick2 == "" || nick2 == null){
+        alert("WYPEŁNIJ WSZYSTKIE POLA")
+        document.getElementById("txt2").innerHTML = ""
+    }
+    if(ogrze2 == "" || ogrze2 == null){
+        alert("WYPEŁNIJ WSZYSTKIE POLA")
+        document.getElementById("txt2").innerHTML = ""
+    }
+}
+function opinia4(){
+    var nick3 = document.getElementById("User3").value
+    var ogrze3 = document.getElementById("ogame3").value
+    document.getElementById("txt3").innerHTML = "Dziękujemy za twoją opinie " + nick3 + ". Miło nam że korzystasz z naszego serwisu"
+    if(nick3 == "" || nick3 == null){
+        alert("WYPEŁNIJ WSZYSTKIE POLA")
+        document.getElementById("txt3").innerHTML = "" 
+    }
+    if(ogrze3 == "" || ogrze3 == null){
+        alert("WYPEŁNIJ WSZYSTKIE POLA")
+        document.getElementById("txt3").innerHTML = ""
+    }
+}
+function opinia5(){
+    var nick4 = document.getElementById("User4").value
+    var ogrze4 = document.getElementById("ogame4").value
+    document.getElementById("txt4").innerHTML = "Dziękujemy za twoją opinie " + nick4 + ". Miło nam że korzystasz z naszego serwisu"
+    if(nick4 == "" || nick4 == null){
+        alert("WYPEŁNIJ WSZYSTKIE POLA")
+        document.getElementById("txt4").innerHTML = ""
+    }
+    if(ogrze4 == "" || ogrze4 == null){
+        alert("WYPEŁNIJ WSZYSTKIE POLA")
+        document.getElementById("txt4").innerHTML = ""
+    }
+}
+function opinia6(){
+    var nick5 = document.getElementById("User5").value
+    var ogrze5 = document.getElementById("ogame5").value
+    document.getElementById("txt5").innerHTML = "Dziękujemy za twoją opinie " + nick5 + ". Miło nam że korzystasz z naszego serwisu"
+    if(nick5 == "" || nick5 == null){
+        alert("WYPEŁNIJ WSZYSTKIE POLA")
+        document.getElementById("txt5").innerHTML = "" 
+    }
+    if(ogrze5 == "" || ogrze5 == null){
+        alert("WYPEŁNIJ WSZYSTKIE POLA")
+        document.getElementById("txt5").innerHTML = ""
+    }
+}
+function opinia7(){
+    var nick6 = document.getElementById("User6").value
+    var ogrze6 = document.getElementById("ogame6").value
+    document.getElementById("txt6").innerHTML = "Dziękujemy za twoją opinie " + nick6 + ". Miło nam że korzystasz z naszego serwisu"
+    if(nick6 == "" || nick6 == null){
+        alert("WYPEŁNIJ WSZYSTKIE POLA")
+        document.getElementById("txt6").innerHTML = "" 
+    }
+    if(ogrze6 == "" || ogrze6 == null){
+        alert("WYPEŁNIJ WSZYSTKIE POLA")
+        document.getElementById("txt6").innerHTML = ""
+    }
+}
